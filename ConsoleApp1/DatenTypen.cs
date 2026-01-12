bc1e4f88bac3b275777276100466944e7ff8b211
Torben <Torben@DESKTOP-EQG19PM>
2026-01-12 19:57:18 +0100

    Add project files.

---
+++ ConsoleApp1/DatenTypen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DaveLernt
+{
+    internal class DatenTypen
+    {
+        public void ShowDataTypes()
+        {
+            //Zeichenketten
+            string abc = "ich bin ein string";
+            char a = 'a';
+            //Nummerische
+            //Ganzzahl
+            Console.WriteLine($"Int: {int.MinValue} - {int.MaxValue}");
+            Console.WriteLine($"Uint: {uint.MinValue} - {uint.MaxValue}");
+            Console.WriteLine($"Nint: {nint.MinValue} - {nint.MaxValue}");
+            short b = 2;
+            Console.WriteLine($"Short: {short.MinValue} - {short.MaxValue}");
+            int c = 3;
+            //Gleitkommazahlen
+            float f = 4.2f;
+            Console.WriteLine($"Float: {float.MinValue} - {float.MaxValue}");
+            double g = 4.478575;
+            Console.WriteLine($"Double: {double.MinValue} - {double.MaxValue}");
+            decimal h = 4.4m;
+            Console.WriteLine($"Decimal: {decimal.MinValue} - {decimal.MaxValue}");
+            //operatischer
+            bool abcd = true;
+            Haarfarben haarfarben = Haarfarben.hdsajkdhasj;
+            var transformer = 2;
+        }
+        public enum Haarfarben
+        {
+            Blond,
+            Schwarz,
+            Rot,
+            Braun,
+            Glatze,
+            DeineMum,
+            hdsajkdhasj
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace DaveLernt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zeichenketten
+            string abc = "ich bin ein string";
+            char a = 'a';
+            //Nummerische
+            //Ganzzahl
+            Console.WriteLine($"Int: {int.MinValue} - {int.MaxValue}");
+            Console.WriteLine($"Uint: {uint.MinValue} - {uint.MaxValue}");
+            Console.WriteLine($"Nint: {nint.MinValue} - {nint.MaxValue}");
+            short b = 2;
+            Console.WriteLine($"Short: {short.MinValue} - {short.MaxValue}");
+            int c = 3;
+            //Gleitkommazahlen
+            float f = 4.2f;
+            Console.WriteLine($"Float: {float.MinValue} - {float.MaxValue}");
+            double g = 4.478575;
+            Console.WriteLine($"Double: {double.MinValue} - {double.MaxValue}");
+            decimal h = 4.4m;
+            Console.WriteLine($"Decimal: {decimal.MinValue} - {decimal.MaxValue}");
+            //operatischer
+            bool abcd = true;
+            Haarfarben haarfarben = Haarfarben.hdsajkdhasj;
+            var transformer = 2;
+        }
+    }
+    public enum Haarfarben
+    {
+        Blond,
+        Schwarz,
+        Rot,
+        Braun,
+        Glatze,
+        DeineMum,
+        hdsajkdhasj
+    }
+}
